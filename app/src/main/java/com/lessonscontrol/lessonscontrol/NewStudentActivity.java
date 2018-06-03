2ae231c1e8a1667a43326d628c39d87707ee96c1
Hayllander Blonski <hayblonski@gmail.com>
2018-06-03 12:15:02 -0300

    Using Android native FAB component, new (empty) activity for adding a student.

---
+++ app/src/main/java/com/lessonscontrol/lessonscontrol/NewStudentActivity.java
@@ -0,0 +1,14 @@
+package com.lessonscontrol.lessonscontrol;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewStudentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_student);
+
+    }
+}
