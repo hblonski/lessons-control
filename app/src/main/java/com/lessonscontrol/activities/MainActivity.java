6df57be1ffd87578e417aa91aedb0205d6c04ff2
hblonski <hayblonski@gmail.com>
2019-01-22 16:48:34 -0200

    Delete button shows on student card right swipe (incomplete - no button action implemented yet).

--- app/src/main/java/com/lessonscontrol/activities/MainActivity.java
+++ app/src/main/java/com/lessonscontrol/activities/MainActivity.java
@@ -3,18 +3,21 @@ package com.lessonscontrol.activities;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
 import com.lessonscontrol.adapter.StudentListAdapter;
 import com.lessonscontrol.data.entities.Student;
 import com.lessonscontrol.data.viewModel.LessonViewModel;
 import com.lessonscontrol.data.viewModel.StudentViewModel;
+import com.lessonscontrol.utils.StudentCardSwipeController;
 
 import java.util.List;
 
@@ -53,6 +56,16 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(studentListAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         this.observeDataChange(studentListAdapter);
+
+        final StudentCardSwipeController studentCardSwipeController = new StudentCardSwipeController();
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(studentCardSwipeController);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+        recyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+                studentCardSwipeController.onDraw(c, getResources());
+            }
+        });
     }
 
     private void observeDataChange(final StudentListAdapter studentListAdapter) {
